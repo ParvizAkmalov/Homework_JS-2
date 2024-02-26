f056c757aa3999512b3418f7a475050e17791673
Parviz_8901 <parvizakmalov39@gmail.com>
2024-02-27 00:09:37 +0500

    Js homework || 2

---
+++ smain/main.js
@@ -0,0 +1,33 @@
+console.log("                                                 1-vazifa");
+
+let str = "aaa@bbb@ccc"
+let stra = "!"
+
+console.log(
+    str.replace("@", stra).replace("@", stra)
+);
+
+
+console.log("                                                 2-vazifa");
+let hello = "HELLOWORD"
+let b = "HELLOWORD"
+
+console.log(
+    hello = "HELLO WORD".toLowerCase()
+);
+
+
+console.log("                                                 3-vazifa");
+let html = "Hello, it is HTML"
+
+console.log(
+    html.replace("HTML", "not JS")
+);
+
+
+console.log("                                                 4-vazifa");
+let a = "alex"
+
+console.log(
+    a.replace("a", "A")
+);
